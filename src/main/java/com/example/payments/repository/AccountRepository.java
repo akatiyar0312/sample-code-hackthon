175aa2e769147edb42143d8c92fa545c775e3da6
Your Name <you@example.com>
2025-07-23 04:47:08 +0530

    final

--- src/main/java/com/example/payments/repository/AccountRepository.java
+++ src/main/java/com/example/payments/repository/AccountRepository.java
@@ -4,9 +4,11 @@ import com.example.payments.model.Account;
 import com.example.payments.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByUser(User user);
 
--- src/main/java/com/example/payments/repository/PayeeRepository.java
+++ src/main/java/com/example/payments/repository/PayeeRepository.java
@@ -3,9 +3,11 @@ package com.example.payments.repository;
 import com.example.payments.model.Payee;
 import com.example.payments.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PayeeRepository extends JpaRepository<Payee, Long> {
     List<Payee> findByUser(User user);
     List<Payee> findByUserId(Long id);
