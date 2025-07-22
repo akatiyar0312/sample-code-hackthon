ebf2f96f46245c14f3a677f618e7de41c21b80cd
Your Name <you@example.com>
2025-07-23 04:36:00 +0530

    updated annotation

--- src/main/java/com/example/payments/Application.java
+++ src/main/java/com/example/payments/Application.java
@@ -2,8 +2,10 @@ package com.example.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.payments")
+@EnableJpaRepositories
 public class Application {
 
 	public static void main(String[] args) {
