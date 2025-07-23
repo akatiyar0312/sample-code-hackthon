87f1fa6231c8993c31cd42bf88caa101b866b693
Your Name <you@example.com>
2025-07-23 23:46:09 +0530

    keeping oly 1 controllder

--- src/main/java/com/example/payments/controller/AuthController.java
+++ src/main/java/com/example/payments/controller/AuthController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.payments.service.UserService;
-
-
 @RestController
 @RequestMapping("/api")
 public class AuthController {
