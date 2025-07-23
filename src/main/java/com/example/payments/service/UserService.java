8cd92458c284340d0c2ecf426e671a129ef67f6a
Your Name <you@example.com>
2025-07-23 23:37:32 +0530

    commit code

--- src/main/java/com/example/payments/service/UserService.java
+++ src/main/java/com/example/payments/service/UserService.java
@@ -3,11 +3,9 @@ package com.example.payments.service;
 import com.example.payments.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
